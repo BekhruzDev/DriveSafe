c83a5e6c432c392dea0fa5c7fab3d6653e3a83ad
bekhruz <bekhruzabdullakhujaevpro@gmail.com>
2023-06-26 12:20:03 +0500

    Basic UI is setUp

---
+++ app/src/main/java/com/example/drivesafe/ui/SampleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.drivesafe.ui
+
+import com.example.drivesafe.databinding.MainLayoutBinding
+import com.example.drivesafe.ui.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SampleActivity:BaseActivity<MainLayoutBinding>(MainLayoutBinding::inflate) {
+
+
+
+}
