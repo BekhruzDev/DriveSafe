48e55e43b7e18c590bf592278382d97340a57219
bekhruz <bekhruzabdullakhujaevpro@gmail.com>
2023-08-20 15:42:42 +0500

    Code Cleaning and Restructuring

---
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -0,0 +1,11 @@
+object ConfigData{
+    const val applicationId = "com.bekhruzdev.drivesafe"
+    const val compileSdk = 34
+    const val buildToolsVersion = "32.0.0"
+    const val targetSdk = 34
+    const val minSdk = 22
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+    const val databaseVersion = 0
+    const val databaseName = "cache_room_database"
+}
---
+++ buildSrc/src/main/kotlin/TestDependencies.kt
@@ -0,0 +1,12 @@
+object TestDependencies {
+    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+    const val rules = "androidx.test:rules:${Versions.rules}"
+    const val runner = "androidx.test:runner:${Versions.runner}"
+    const val extJunit = "androidx.test.ext:junit:${Versions.extJunit}"
+    const val testCore = "androidx.test:core:${Versions.testCore}"
+    const val testKtx = "androidx.test:core-ktx:${Versions.testKtx}"
+    const val junitKtx = "androidx.test.ext:junit-ktx:${Versions.junitKtx}"
+    const val jUnit = "junit:junit:${Versions.jUnit}"
+    const val espressoContrib = "androidx.test.espresso:espresso-contrib:${Versions.espressoContrib}"
+    const val jUnitRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
