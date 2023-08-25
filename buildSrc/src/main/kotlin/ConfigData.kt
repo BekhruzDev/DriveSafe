6f178ae08694e9080e250505311f1a5f493ced71
bekhruz <bekhruzabdullakhujaevpro@gmail.com>
2023-08-25 16:52:14 +0500

    versionCode = 8

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -4,8 +4,8 @@ object ConfigData{
     const val buildToolsVersion = "32.0.0"
     const val targetSdk = 34
     const val minSdk = 22
-    const val versionCode = 7
-    const val versionName = "1.0.8"
+    const val versionCode = 8
+    const val versionName = "1.0.9"
     const val databaseVersion = 0
     const val databaseName = "cache_room_database"
 }
